716b46019f2a5f5c50e1d2692f4e212576c5c895
odedpeer <peer.oded@gmail.com>
2014-09-13 21:09:37 +0300

    fix encoding problem after adding spring security

--- src/main/java/com/traffitruck/Application.java
+++ src/main/java/com/traffitruck/Application.java
@@ -1,29 +1,15 @@
 package com.traffitruck;
 
-import javax.servlet.Filter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 
 @EnableAutoConfiguration
 @ComponentScan
-@Configuration
 public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
-    }
-    
-    @Bean
-    public Filter handleEncoding() {
-    	CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
-        return filter;
     }
 }
--- src/main/java/com/traffitruck/WebSecurityConfig.java
+++ src/main/java/com/traffitruck/WebSecurityConfig.java
@@ -6,13 +6,22 @@ import org.springframework.security.config.annotation.authentication.configurers
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	// handle content encoding
+    	CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter,CsrfFilter.class);
+        
         http
             .authorizeRequests()
                 .antMatchers("/css/**", "/images/**").permitAll()
