63c2e2ac85cef83e392c51785c0fe030f8ea91b4
odedpeer <peer.oded@gmail.com>
2014-09-13 13:13:13 +0300

    show loads in HTML table

--- src/main/java/com/traffitruck/web/HtmlController.java
+++ src/main/java/com/traffitruck/web/HtmlController.java
@@ -1,8 +1,14 @@
 package com.traffitruck.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.traffitruck.domain.Load;
 import com.traffitruck.service.MongoDAO;
 
 @RestController
@@ -10,4 +16,11 @@ public class HtmlController {
 
 	@Autowired
 	private MongoDAO dao;
+	
+    @RequestMapping("/loads")
+    ModelAndView greetings(@RequestParam(value="name", required=false) String name) {
+    	List<Load> loads = dao.getLoads();
+        return new ModelAndView("show_loads", "loads", loads);
+    }
+
 }
