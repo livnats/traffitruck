d7ccdff9316ba59caf7656e8371771f5b08fe347
peero1 <peero1@isxxpeero1l3c.corp.emc.com>
2016-04-25 00:49:51 +0300

    add command line utility to create admins upgrade spring boot to 1.3.3

---
+++ src/main/java/com/traffitruck/AdminCreator.java
@@ -0,0 +1,61 @@
+package com.traffitruck;
+
+import static com.traffitruck.domain.Role.ADMIN;
+
+import java.util.Arrays;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.traffitruck.domain.LoadsUser;
+import com.traffitruck.service.MongoDAO;
+
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
+@Profile("adminCreator")
+public class AdminCreator implements CommandLineRunner {
+
+    private static final Log logger = LogFactory.getLog(AdminCreator.class);
+
+    @Autowired
+    private MongoDAO dao;
+
+    public static void main(String[] args) throws Exception {
+	SpringApplication app = new SpringApplication(AdminCreator.class);
+	app.setWebEnvironment(false);
+	app.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+	logger.info("ODED " + Arrays.asList(args));
+
+	if (args.length != 3) {
+	    logger.error("Usage: mvn spring-boot:run -P adminCreator -Drun.arguments=\"<username>,<password>\"");
+	    System.exit(1);
+	}
+
+	// args[0] is the active Spring boot profile
+	String username = args[1];
+	String password = args[2];
+
+	LoadsUser user = new LoadsUser();
+	user.setRole(ADMIN);
+	user.setUsername(username);
+	user.setPassword(password);
+
+	dao.storeUser(user);
+	logger.info("Created user " + username);
+	System.exit(0);
+    }
+
+}
--- src/main/java/com/traffitruck/Application.java
+++ src/main/java/com/traffitruck/Application.java
@@ -3,10 +3,12 @@ package com.traffitruck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 
 
 @EnableAutoConfiguration
 @ComponentScan
+@Profile("web")
 public class Application {
 
     public static void main(String[] args) throws Exception {
