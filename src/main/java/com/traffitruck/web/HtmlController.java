49ae09a4cc9b08fd2c5d3d249eb90be31e6954ee
odedpeer <peer.oded@gmail.com>
2016-04-25 21:37:03 +0300

    fix admin menu

--- src/main/java/com/traffitruck/web/HtmlController.java
+++ src/main/java/com/traffitruck/web/HtmlController.java
@@ -39,7 +39,7 @@ public class HtmlController {
 
 	@Autowired
 	private MongoDAO dao;
-	
+
     @RequestMapping("/login")
     ModelAndView login() {
         return new ModelAndView("login");
@@ -60,7 +60,7 @@ public class HtmlController {
         model.put("trucks", dao.getTrucksWithoutImages());
         return new ModelAndView("show_trucks", model);
     }
-    
+
     @RequestMapping({"/findTrucksForLoad"})
     ModelAndView findTrucksForLoad() {
         Map<String, Object> model = new HashMap<>();
@@ -70,12 +70,7 @@ public class HtmlController {
         model.put("trucks", dao.getTrucksForUserAndRegistration(username, TruckRegistrationStatus.APPROVED));
         return new ModelAndView("find_load_for_trucks", model);
     }
-    
-    @RequestMapping({"/"})
-    ModelAndView showMain() {
-        return new ModelAndView("main");
-    }
-    
+
     @RequestMapping(value = "/newload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ModelAndView newLoad(@ModelAttribute("load") Load load) {
     	load.setCreationDate(new Date());
@@ -102,7 +97,7 @@ public class HtmlController {
         model.put("loads", dao.getLoadsForUser(username));
         return new ModelAndView("my_loads", model);
     }
-    
+
     @RequestMapping(value = "/registerUser", method = RequestMethod.GET)
     ModelAndView registerUser() {
         return new ModelAndView("register_user");
@@ -117,7 +112,7 @@ public class HtmlController {
         model.put("trucks", dao.getMyApprovedTrucksId(username));
         return new ModelAndView("add_availability", model);
     }
-    
+
     @RequestMapping(value = "/addAvailability", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     ModelAndView addAvailability(@RequestParam("truckId") String truckId,
     					  		 @RequestParam("source") String source,
@@ -125,7 +120,7 @@ public class HtmlController {
     					  		 @RequestParam("availTime") String availTime,
     					  		 @RequestParam("dateAvail") String dateAvail,
     					  		 @RequestParam("hourAvail") String hourAvail) throws IOException, ParseException{
-        
+
     	TruckAvailability truckAvail = new TruckAvailability();
     	truckAvail.setTruckId(truckId);
     	truckAvail.setSource(source);
@@ -144,31 +139,31 @@ public class HtmlController {
     	dao.storeTruckAvailability(truckAvail);
         return new ModelAndView("redirect:/addAvailability");
     }
-    
+
 	@RequestMapping(value = "/registerUser", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     ModelAndView registerUser(@ModelAttribute("user") LoadsUser user) {
         dao.storeUser(user);
 		SecurityContextHolder.getContext().setAuthentication(
 				new UsernamePasswordAuthenticationToken(
-						user.getUsername(), 
-						"", 
+						user.getUsername(),
+						"",
 						Collections.singletonList(new SimpleGrantedAuthority(user.getRole().toString()))));
         return new ModelAndView("redirect:" + user.getRole().getLandingUrl());
     }
-    
+
     @RequestMapping(value = "/deleteLoad", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     ModelAndView deleteLoad(String loadId) {
         dao.deleteLoadById(loadId);
         return new ModelAndView("redirect:/myLoads");
     }
-    
+
     @RequestMapping(value = "/truckApproval", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     ModelAndView approveTruck(@ModelAttribute("truck") Truck truck) {
     	truck.setRegistrationStatus(TruckRegistrationStatus.APPROVED);
         dao.updateTruck(truck);
         return new ModelAndView("redirect:/");
     }
-    
+
     @RequestMapping(value = "/myTrucks")
     ModelAndView myTrucks() {
     	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -178,14 +173,17 @@ public class HtmlController {
         model.put("trucks", dao.getTrucksForUser(username));
         return new ModelAndView("my_trucks", model);
     }
-    
+
     @RequestMapping(value = "/truckerMenu")
     ModelAndView truckerMenu() {
-        Map<String, Object> model = new HashMap<>();
-        return new ModelAndView("trucker_menu", model);
+	return new ModelAndView("trucker_menu");
     }
-    
-   
+
+    @RequestMapping(value = "/adminMenu")
+    ModelAndView adminMenu() {
+	return new ModelAndView("admin_menu");
+    }
+
     @RequestMapping("/nonApprovedTrucks")
     ModelAndView showNonApprovedTrucks() {
     	Map<String, Object> model = new HashMap<>();
@@ -193,7 +191,7 @@ public class HtmlController {
         model.put("trucks", dao.getNonApprovedTrucks());
         return new ModelAndView("show_non_approved_trucks", model);
     }
-    
+
 	@RequestMapping(value = "/approval/licenseimage/{truckId}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
 	public byte[] getUser(@PathVariable String truckId) {
 		return dao.getTruckById(truckId).getVehicleLicensePhoto().getData();
@@ -206,17 +204,17 @@ public class HtmlController {
         model.put("truck", dao.getTruckByIdWithoutImages(id));
         return new ModelAndView("truck_approval", model);
     }
-    
+
     @RequestMapping("/newTruck")
     ModelAndView newTruck() {
         return new ModelAndView("new_truck");
     }
-    
+
     @RequestMapping(value = "/newTruck", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ModelAndView newTruck(@RequestParam("licensePlateNumber") String licensePlateNumber,
     					  @RequestParam("truckPhoto") MultipartFile truckPhoto,
     		              @RequestParam("vehicleLicensePhoto") MultipartFile vehicleLicensePhoto) throws IOException{
-        
+
     	Truck truck = new Truck();
     	truck.setLicensePlateNumber(licensePlateNumber);
     	truck.setVehicleLicensePhoto(new Binary(vehicleLicensePhoto.getBytes()));
@@ -229,5 +227,5 @@ public class HtmlController {
     	dao.storeTruck(truck);
         return new ModelAndView("redirect:/myTrucks");
     }
-    
+
 }
---
+++ src/main/resources/templates/admin_menu.ftl
@@ -0,0 +1,42 @@
+<html dir="rtl">
+	<head>
+	    <title>TraffiTruck</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="/css/traffitruck.css">
+		<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+	</head>
+	<body>
+	<div id="main">
+		<div id="title">
+			<img src="/images/truck-blue.jpg" width="15%"/>
+			<img src="/images/logo.jpg" width="20%"/>
+		</div>
+		<div id="body">
+			<div id="sidebar">
+				<p>&nbsp;</p>
+			</div>
+			<div id="content">
+				<h2>עמוד ראשי</h2>
+				<table border="0">
+					<tr> 
+						<td>
+							<a href="/nonApprovedTrucks">אשר משאיות</a>
+						</td>
+					</tr>
+					<tr> 
+						<td>
+							<a href="/loads">כל המטענים</a>
+						</td>
+					</tr>
+					<tr> 
+						<td>
+							<a href="/trucks">כל המשאיות</a>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+	</body>
+</html>
+				
