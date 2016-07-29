f95063822c2ae3e31288cf173bda05088845e52d
Livnat Peer <livnats@gmail.com>
2016-07-29 08:27:06 +0300

    cache

--- src/main/java/com/traffitruck/WebSecurityConfig.java
+++ src/main/java/com/traffitruck/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,6 +39,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		factory.setMaxFileSize("5MB");
 		factory.setMaxRequestSize("10MB");
 		return factory.createMultipartConfig();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		super.configure(web);
+		web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
 	}
 	
     @Override
