8cbdbfd5debcc5138c8abeb00578471eba856ce9
livnats <livnats@gmail.com>
2016-08-31 20:34:27 +0300

    few changes to install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 sudo su -
-yum install git
+yum install -y git
 cat > /etc/yum.repos.d/mongodb.repo
 
 [MongoDB]
@@ -8,7 +8,7 @@ baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64
 gpgcheck=0
 enabled=1
 
-yum install mongodb-org
+yum install -y mongodb-org
 mkdir -p /data/db
 mongod &
 
@@ -21,7 +21,7 @@ git clone https://github.com/livnats/traffitruck.git
 wget http://apache.spd.co.il/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 sudo tar -zxvf apache-maven-3.3.9-bin.tar.gz -C /opt
 sudo ln -s /opt/apache-maven-3.3.9/bin/mvn /bin/mvn
-
+rm -rf http://apache.spd.co.il/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 
 sudo yum install -y java-1.8.0-openjdk-devel.x86_64
 sudo alternatives --install /usr/bin/java java /usr/lib/jvm/java-1.8.0-openjdk/bin/java 20000
@@ -29,7 +29,8 @@ sudo alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-1.8.0-openjdk
 sudo /usr/sbin/alternatives --config java
 sudo /usr/sbin/alternatives --config javac
 
-sudo rm /usr/java/latest
+#sudo rm /usr/jaiva/latest
+sudo mkdir /usr/java
 sudo ln -s /usr/lib/jvm/java-1.8.0-openjdk /usr/java/latest
 
 #echo "export JAVA_HOME=/usr/java/jdk1.7.0_55" >> ~/.bashrc
